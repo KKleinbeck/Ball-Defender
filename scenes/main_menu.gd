dd0f7864edb63a225c842a380ea6f74191df6dbb
Kevin Kleinbeck <kekleinbeck@gmail.com>
2025-04-19 09:11:00 +0200

    Added localisation file

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -5,6 +5,10 @@ func _ready() -> void:
 	for _i in 5:
 		spawnRandomBall()
 	
+	#var translation = load("res://localisations.csv")
+	#TranslationServer.add_translation(translation)
+	TranslationServer.set_locale(OS.get_locale_language())
+	
 	%OptionEndless.pressed.connect(_on_endless_start)
 	%OptionDaily.pressed.connect(_debug_info)
 	%OptionUpgrades.pressed.connect(_enter_upgrade_store)
